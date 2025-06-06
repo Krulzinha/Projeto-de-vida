09b2f38a282097a9b9af2ef635b5a19e4383d9f0
Krulzinha <137783030+Krulzinha@users.noreply.github.com>
2025-06-06 08:55:15 -0300

    editado

--- main.js
+++ main.js
@@ -10,11 +10,30 @@ botoes[i].onclick= function () {
 }
 
     botoes[i].classList.add("ativo");
-    textos[j].classList.remove("ativo");
+    textos[j].classList.add("ativo");
 }
 }
 
-    
+    const contadores= document.querySelectorAll("contador");
+    const tempoObjetivo= new Date("2025-12-12T00:06:00");
+
+
+    contadores[0]. textContent= calculaTempo(tempoObjetivo1);
+
+    fuction calculaTempo(tempoObjetivo) {
+        let tempoAtual = new Date();
+        let tempoFinal =tempoObjetivo - tempoAtual;
+        let segundos = Math.floor(tempoFinal / 1000);
+        let minutos = Math.floor(segunods / 60);
+        let horas = Math.floor(minutos / 60);
+        let dias = Math.floor(horas / 24);
+
+        segundos %= 60;
+        minutos %= 60;
+        horas %= 24;
+
+        return dias + "dias " + horas + "horas" + minutos + " minutos " + segundos " segundos ";
+    }
 
 
 
